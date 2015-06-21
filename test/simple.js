1189f4408d1d147e420da8fc1f3d12cb39cf5a8d
Linus Unnebäck <linus@folkdatorn.se>
2015-06-21 19:37:57 +0200

    coding style: standard

--- test/simple.js
+++ test/simple.js
@@ -1,42 +1,37 @@
+/* eslint-env mocha */
 
-var fs = require('fs');
-var assert = require('assert');
-var captureWindow = require('..');
+var fs = require('fs')
+var assert = require('assert')
+var captureWindow = require('../')
 
 describe('captureWindow', function () {
-
-  var cleanup = [];
+  var cleanup = []
 
   afterEach(function () {
-    cleanup.forEach(fs.unlinkSync.bind(fs));
-    cleanup = [];
-  });
+    cleanup.forEach(fs.unlinkSync.bind(fs))
+    cleanup = []
+  })
 
   it('captures a window', function (done) {
-
     captureWindow('Window Server', 'Menubar', function (err, filePath) {
+      assert.ifError(err)
 
-      assert.ifError(err);
-      cleanup.push(filePath);
+      cleanup.push(filePath)
 
       fs.readFile(filePath, function (err, data) {
+        assert.ifError(err)
 
-        assert.ifError(err);
-        assert.equal(data[0], 0x89);
-        assert.equal(data[1], 0x50);
-        assert.equal(data[2], 0x4E);
-        assert.equal(data[3], 0x47);
-        assert.equal(data[4], 0x0D);
-        assert.equal(data[5], 0x0A);
-        assert.equal(data[6], 0x1A);
-        assert.equal(data[7], 0x0A);
+        assert.equal(data[0], 0x89)
+        assert.equal(data[1], 0x50)
+        assert.equal(data[2], 0x4E)
+        assert.equal(data[3], 0x47)
+        assert.equal(data[4], 0x0D)
+        assert.equal(data[5], 0x0A)
+        assert.equal(data[6], 0x1A)
+        assert.equal(data[7], 0x0A)
 
-        done();
-
-      });
-
-    });
-
-  });
-
-});
+        done()
+      })
+    })
+  })
+})
