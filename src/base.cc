119d95a9c70556056a3e056c354b84c3f413bcd4
Linus Unnebäck <linus@folkdatorn.se>
2015-08-22 19:54:24 +0200

    lib: update to nan 2.x

--- src/base.cc
+++ src/base.cc
@@ -1,40 +1,35 @@
 
-class CaptureWindowWorker : public NanAsyncWorker {
+class CaptureWindowWorker : public Nan::AsyncWorker {
 public:
-  CaptureWindowWorker (v8::Local<v8::Value> bundle, v8::Local<v8::Value> title, v8::Local<v8::Value> path, NanCallback *callback)
-    : NanAsyncWorker(callback), bundle(bundle), title(title), path(path) {}
+  CaptureWindowWorker (v8::Local<v8::Value> bundle, v8::Local<v8::Value> title, v8::Local<v8::Value> path, Nan::Callback *callback)
+    : Nan::AsyncWorker(callback), bundle(bundle), title(title), path(path) {}
   ~CaptureWindowWorker () {}
 
   void Execute ();
 
   void HandleOKCallback () {
-    NanScope();
-
     v8::Local<v8::Value> argv[] = {
-      NanNull(),
-      NanNew(*path)
+      Nan::Null(),
+      Nan::New(*path).ToLocalChecked()
     };
 
     callback->Call(2, argv);
   }
 
 private:
-  NanUtf8String bundle;
-  NanUtf8String title;
-  NanUtf8String path;
+  Nan::Utf8String bundle;
+  Nan::Utf8String title;
+  Nan::Utf8String path;
 };
 
 NAN_METHOD(capture_window) {
-  NanScope();
-
-  NanCallback *cb = new NanCallback(args[3].As<v8::Function>());
-  NanAsyncQueueWorker(new CaptureWindowWorker(args[0], args[1], args[2], cb));
-
-  NanReturnUndefined();
+  Nan::Callback *cb = new Nan::Callback(info[3].As<v8::Function>());
+  Nan::AsyncQueueWorker(new CaptureWindowWorker(info[0], info[1], info[2], cb));
 }
 
-void Initialize(v8::Handle<v8::Object> exports) {
-  exports->Set(NanNew("captureWindow"), NanNew<v8::FunctionTemplate>(capture_window)->GetFunction());
+NAN_MODULE_INIT(Initialize) {
+  Nan::Set(target, Nan::New("captureWindow").ToLocalChecked(),
+    Nan::GetFunction(Nan::New<v8::FunctionTemplate>(capture_window)).ToLocalChecked());
 }
 
 NODE_MODULE(capture_window, Initialize)
